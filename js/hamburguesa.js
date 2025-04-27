8719b45c3b89e2a68d7de7b42b0572bab9b00136
Luis <luisjimenezamo@gmail.com>
2025-04-27 23:00:06 +0200

    FinalFinalFinal

--- js/hamburguesa.js
+++ js/hamburguesa.js
@@ -4,15 +4,16 @@
         const movil = document.querySelector('.header2__movil');
         const cerrar = document.querySelector('.header2__movil-cerrar');
 
+        // Cambiar clase a activo y bloquear scroll de página una vez aparezca el menú
         boton.addEventListener('click', () => {
             movil.classList.toggle('activo');
             document.body.style.overflow = 'hidden';
         });
-
+        // Añadir clase cerrando y reestablecer scroll de página una vez se cierra el menú
         cerrar.addEventListener('click', () => {
             movil.classList.add('cerrando');
-            document.body.style.overflow = '';
-
+            document.body.style.overflow = '';ç
+        // Esperar medio segundo antes de cerrar para ver animación
             setTimeout(() => {
                 movil.classList.remove('activo', 'cerrando');
             }, 500);
